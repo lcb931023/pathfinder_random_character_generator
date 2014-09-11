3c05ad3290073f4a5020f7c029c778bba7b2e0a1
Changbai Li <lcb931023@gmail.com>
2014-09-11 13:46:51 -0400

    Initial button for generator

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+// main
+$( document ).ready(function() {
+  
+  $('#btn_generate').click(function (event) {
+    event.preventDefault();
+    console.log("blablabal");
+    // [TODO] generate and change DOM
+    
+  });
+  
+});
