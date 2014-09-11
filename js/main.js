804a05335d2cec405d61fa310629fbf0fca7cd15
Changbai Li <lcb931023@gmail.com>
2014-09-11 19:19:24 -0400

    hooking up the json started

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 // main
+var testVar;
+
 $( document ).ready(function() {
   
   $('#btn_generate').click(function (event) {
     event.preventDefault();
     console.log("blablabal");
-    // [TODO] generate and change DOM
-    
+    // generate and change DOM
+    generateChar();
   });
   
 });
+
+function generateChar() {
+  $.getJSON("../json/races.json", function(json) {
+    testVar = json.races; // debugging
+    console.log(testVar);
+    // [TODO] make these attributes into a "character" object that carries them.
+    var pRace = json.races[Math.floor(Math.random() * json.races.length)];
+    
+    $('#data-race').html(pRace.name);
+    
+  });
+}
 
 //race 
 //class
