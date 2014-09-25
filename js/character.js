e0ad6baedbdcd3e50799f637fc243548ad6a7538
lcb931023 <lcb931023@gmail.com>
2014-09-25 10:19:25 -0400

    Angular JS overhaul complete

--- js/character.js
+++ js/character.js
@@ -1,11 +1,43 @@
 angular.module('character', [])
-  .controller('CharacterController', function() {
+  .controller('CharacterController', function($scope, $http) {
 
-    this.str = Math.floor(Math.random() * 10) + 10;
-    this.dex = Math.floor(Math.random() * 10) + 10;
-    this.con = Math.floor(Math.random() * 10) + 10;
-    this.int = Math.floor(Math.random() * 10) + 10;
-    this.wis = Math.floor(Math.random() * 10) + 10;
-    this.cha = Math.floor(Math.random() * 10) + 10;
+    $http.get('json/races.json')
+      .then(function(res){
+        this.raceJson = res.data;
+      }
+    );
+
+    this.generate = function generate($http) {
+
+      // Placeholder Values
+      this.str = Math.floor(Math.random() * 10) + 10;
+      this.dex = Math.floor(Math.random() * 10) + 10;
+      this.con = Math.floor(Math.random() * 10) + 10;
+      this.int = Math.floor(Math.random() * 10) + 10;
+      this.wis = Math.floor(Math.random() * 10) + 10;
+      this.cha = Math.floor(Math.random() * 10) + 10;
+      // From Races
+      
+      this.race = raceJson.races[Math.floor(Math.random() * raceJson.races.length)];
+        
+      this.attributeMods =
+      {
+        "str":0,
+        "dex":0,
+        "con":0,
+        "int":0,
+        "wis":0,
+        "cha":0,
+      };
+      var pAbility = this.race.ability;
+      // Assign mods from json to attributes of attributeMods
+        // ugh vocabulary
+      for(var i = 0; i < pAbility.length; i++){
+        var tKey = Object.keys(pAbility[i]);
+        this.attributeMods[tKey] = pAbility[i][tKey];
+      }
+
+      // From Classes
+    };
 
   });
