d46c5dd2159c25a89794d25298afd30da0b64e9e
lcb931023 <lcb931023@gmail.com>
2014-09-25 12:36:12 -0400

    Class integration started. Reformatting class json so it's easier to be processed

--- js/character.js
+++ js/character.js
@@ -6,6 +6,11 @@ angular.module('character', [])
         this.raceJson = res.data;
       }
     );
+		$http.get('json/classes.json')
+      .then(function(res){
+        this.classJson = res.data;
+      }
+    );
 
     this.generate = function generate($http) {
 
@@ -16,18 +21,12 @@ angular.module('character', [])
       this.int = Math.floor(Math.random() * 10) + 10;
       this.wis = Math.floor(Math.random() * 10) + 10;
       this.cha = Math.floor(Math.random() * 10) + 10;
+				
       // From Races
-      
       this.race = raceJson.races[Math.floor(Math.random() * raceJson.races.length)];
-        
       this.attributeMods =
       {
-        "str":0,
-        "dex":0,
-        "con":0,
-        "int":0,
-        "wis":0,
-        "cha":0,
+        "str":0, "dex":0, "con":0, "int":0, "wis":0, "cha":0,
       };
       var pAbility = this.race.ability;
       // Assign mods from json to attributes of attributeMods
@@ -38,6 +37,7 @@ angular.module('character', [])
       }
 
       // From Classes
+			this.class = classJson.Classes[Math.floor(Math.random() * classJson.Classes.length)];
     };
 
   });
